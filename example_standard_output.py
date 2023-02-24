2c6c9c95116c7d60edbbf6671a2f740a9fb70e30
hyeongcheol.park <hyeongcheol.park@straffic.co.kr>
2023-02-24 12:59:17 +0900

    표준출력 예제 작성

---
+++ example_standard_output.py
@@ -0,0 +1,26 @@
+# 기본적인 표준출력 예시
+print("사과는 과일이다.")
+print("바나나도 과일이다.")
+
+# 표준출력에서 여러 문자열을 출력하는 예시
+print("아래처럼 문자열을 연결할 수 있다.")
+print("사과는 과일이다." + "바나나도 과일이다.")
+print("사과는 과일이다." "바나나도 과일이다.")
+
+# 마지막에 출력할 내용 설정 예시
+print("이렇게 하면 줄바꿈 되지 않는다.", end="")
+print("입력된 내용 끝에 특정 값을 출력하고 싶다.", end=" 이렇게 가능하다")
+
+# 문자열이 담긴 변수를 출력하는 예시
+stringApple = "사과"
+stringBanana = "바나나"
+print("이렇게 변수에 담긴 내용도 출력할 수 있다." + stringApple)
+print("이렇게 여러 변수도 출력할 수 있다.")
+print(stringApple + stringBanana)
+
+# 정수값이 담긴 변수를 출력하는 예시
+intCountApple = 3
+intCountBanana = 4
+print(intCountApple)
+print(intCountBanana)
+print(intCountApple + intCountBanana)
