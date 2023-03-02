8002f4728c74bab64be7c3cd807a25a4dbaa3041
hyeongcheol.park <hyeongcheol.park@straffic.co.kr>
2023-03-02 12:25:51 +0900

    제어문 for 작성 예제

---
+++ example_for.py
@@ -0,0 +1,9 @@
+example_list = ["a", "b", "c"]
+
+for item in example_list:
+    print(item)
+
+example_tuple = (("a", "1"), ("b", "2"), ("c", "3"))
+
+for (one, two) in example_tuple:
+    print(one + " " + two)
