85e1cc95adb2ee6e6fe8c4fb97793d357e6661e1
hyeongcheol.park <hyeongcheol.park@straffic.co.kr>
2023-03-06 12:58:12 +0900

    elif 문법 추가

--- example_if.py
+++ example_if.py
@@ -1,9 +1,11 @@
 a = 10
 
-if a > 0:
+if a >= 0:
     print("a는 정수입니다.")
     if a > 5:
         print("a는 5를 초과합니다.")
-else:
+elif a < 0:
     print("a는 음수입니다.")
+else:
+    print("뭔가 잘 못 되었습니다.")
 
