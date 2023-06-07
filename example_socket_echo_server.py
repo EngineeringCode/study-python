5318871fd0d34281421dfc1cf11c8e37103e5bd1
hyeongcheol.park <hyeongcheol.park@straffic.co.kr>
2023-06-08 01:10:01 +0900

    오타 수정

--- example_socket_echo_server.py
+++ example_socket_echo_server.py
@@ -15,5 +15,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         print(f'클라이언트{address}가 접속했습니다: ')
         while True:
             received_data = connection.recv(1024).decode('utf-8')
-            print(f'수시한 데이터: {received_data}')
+            print(f'수신한 데이터: {received_data}')
             connection.sendall(f'{address}: {received_data}'.encode('utf-8'))
